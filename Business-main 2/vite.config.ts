add52959d7bc4a55e9b54a460eb421b64796f42e
venturexbitnoidaedc <venturex.bitnoida.edc@gmail.com>
2026-09-10 13:30:08 +0530

    Update vite.config.ts

--- Business-main 2/vite.config.ts
+++ Business-main 2/vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/venturex/',
+  base: '/vx/',
   plugins: [react()],
   resolve: {
     alias: {
