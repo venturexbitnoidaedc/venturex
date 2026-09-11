ef861eaec767c966aea3d4d4f2f07c2ac719e2df
venturexbitnoidaedc <venturex.bitnoida.edc@gmail.com>
2026-09-12 00:36:41 +0530

    Update App.tsx

--- Business-main 2/src/App.tsx
+++ Business-main 2/src/App.tsx
@@ -1,7 +1,7 @@
 
 import { useEffect, useState, type ReactNode } from 'react';
 import ventureLogo from '../bit-mesra-logo.jpg';
-import ventureXLogo from '../Venture X logo.png';
+import ventureXLogo from '../image (2).png';
 import {
   ArrowDownRight, ArrowRight, Award, BarChart3, CalendarDays, Check,
   ChevronDown, Clock3, ExternalLink, Facebook, Globe2, Instagram, Lightbulb,
